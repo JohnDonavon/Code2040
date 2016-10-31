5bfbd209cc9d7060fbbb95712d71dcd29346f413
John Stephens <john.stephens95@gmail.com>
2016-10-31 16:16:01 -0400

    Fetched the string, reversed it, and sent it back

---
+++ part2.py
@@ -0,0 +1,16 @@
+import urllib
+import urllib2
+
+fetchUrl = "http://challenge.code2040.org/api/reverse"
+validateUrl = "http://challenge.code2040.org/api/reverse/validate"
+
+data = urllib.urlencode({'token':'76e4355f17281fc74fbda85754ed1e3e'})
+
+response = urllib2.urlopen(fetchUrl,data)
+string = response.read()
+reverseString = string[::-1]
+
+postData = urllib.urlencode({'token':'76e4355f17281fc74fbda85754ed1e3e','string':reverseString})
+post = urllib2.urlopen(validateUrl, postData)
+
+print(string, reverseString)
