6c185437a0c99f9515856d925300a566957d542c
John Stephens <john.stephens95@gmail.com>
2016-10-31 16:03:30 -0400

    Added functionality to send a post request to the proper place.

--- part1.py
+++ part1.py
@@ -1,4 +1,9 @@
-import requests
+import urllib
+import urllib2
 
-request = requests.post("http://challenge.code2040.org/api/register", data = {'token':'76e4355f17281fc74fbda85754ed1e3e', 'github':'https://github.com/JohnDonavon/Code2040'})
+url = "http://challenge.code2040.org/api/register"
+data = urllib.urlencode({'token':'76e4355f17281fc74fbda85754ed1e3e', 'github':'https://github.com/JohnDonavon/Code2040'})
 
+response = urllib2.urlopen(url,data)
+
+print(response)
