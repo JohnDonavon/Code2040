92c037c5fd1fba46b526f61265534d706994eb59
John Stephens <john.stephens95@gmail.com>
2016-10-31 16:38:42 -0400

    Finished prefix matching.

---
+++ part4.py
@@ -0,0 +1,20 @@
+import urllib
+import urllib2
+import ast
+
+fetchUrl = "http://challenge.code2040.org/api/prefix"
+validateUrl = "http://challenge.code2040.org/api/prefix/validate"
+
+token = urllib.urlencode({'token':'76e4355f17281fc74fbda85754ed1e3e'})
+response = urllib2.urlopen(fetchUrl, token)
+collection = ast.literal_eval(response.read())
+
+prefix = collection["prefix"]
+array = collection["array"]
+ansArray = []
+for i in array:
+	if not i.startswith(prefix):
+		ansArray.append(i)
+
+postData = urllib.urlencode({'token':'76e4355f17281fc74fbda85754ed1e3e','array':ansArray})
+post = urllib2.urlopen(validateUrl, postData)
