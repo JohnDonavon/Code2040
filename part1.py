4ddea0da1e7e5aa5d3f3fe58af022befaa306714
John Stephens <john.stephens95@gmail.com>
2016-10-31 14:02:41 -0400

    Made a request post.

---
+++ part1.py
@@ -0,0 +1,4 @@
+import requests
+
+request = requests.post("http://challenge.code2040.org/api/register", data = {'token':'76e4355f17281fc74fbda85754ed1e3e', 'github':'https://github.com/JohnDonavon/Code2040'})
+
